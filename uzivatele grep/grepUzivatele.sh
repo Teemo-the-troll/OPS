648aef17f4fe052e098cefc8cdb1a4d9028e5188
root <root@note01-PC.localdomain>
2020-09-24 09:44:28 +0200

    Final works
    grep uzivatele 1

--- uzivatele grep/grepUzivatele.sh
+++ uzivatele grep/grepUzivatele.sh
@@ -1,4 +1,8 @@
 filename=$1
 grp=$(grep -i '^David' $1)
-echo $grp
-cut -d ';' <<< $grp
+echo -n "Jmeno: "
+cut -d ';' -f 1 <<< $grp
+echo -n "Datum narozeni: "
+cut -d ';' -f 2 <<< $grp
+echo -n "Odevzdanych ukolu: "
+cut -d ';' -f 3 <<< $grp
